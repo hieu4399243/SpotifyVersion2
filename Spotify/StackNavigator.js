27c747067086fe1ff02c9c09c301307cff39079f
hieu4399243 <hieu243268@gmail.com>
2023-10-02 12:09:57 +0700

    bottom clor

--- Spotify/StackNavigator.js
+++ Spotify/StackNavigator.js
@@ -1,4 +1,4 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from './screens/HomeScreen';
 import { AntDesign } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
@@ -10,42 +10,63 @@ import ProfileSceen from './screens/ProfileSceen';
 
 const Tab = createBottomTabNavigator();
 
-function BottomTabs (){
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name='Home' 
-                component={HomeScreen} 
+function BottomTabs() {
+    return (
+        <Tab.Navigator screenOptions={{
+            tabBarStyle:{
+                backgroundColor:"green",
+                position:"absolute",
+                bottom:0,
+                left:0,
+                right:0,
+                shadowOpacity:4,
+                shadowRadius:4,
+                elevation:4,
+                shadowOffset: {
+                    width:0,
+                    height:-4
+                },
+                borderTopWidth:0
+            }
+        }}>
+            <Tab.Screen name='Home'
+                component={HomeScreen}
                 options={{
-                tabBarLabel:"Home",
-                headerShown:false,
-                tabBarLabelStyle:{color:"white"},
-                tabBarIcon:({focused})=>
-                focused ? (
-                    <Entypo name="home" size={24} color="black" />
-                ) : (
-                    <AntDesign name="home" size={24} color="black" />
-                )
-            }}/>
+                    tabBarLabel: "Home",
+                    headerShown: false,
+                    tabBarLabelStyle: { color: "white" },
+                    tabBarIcon: ({ focused }) =>
+                        focused ? (
+                            <Entypo name="home" size={24} color="black" />
+                        ) : (
+                            <AntDesign name="home" size={24} color="black" />
+                        )
+                }} />
             <Tab.Screen name='Profile' component={ProfileSceen} options={{
-                tabBarLabel:"Profile",
-                headerShown:false,
-                tabBarLabelStyle:{color:"white"},
-                tabBarIcon:({focused}) => 
-                focused ? (
-                    <Ionicons name="person" size={24} color="black" /> 
-                ) : (
-<Ionicons name="person-outline" size={24} color="black" />                )
-            }}/>
+                tabBarLabel: "Profile",
+                headerShown: false,
+                tabBarLabelStyle: { color: "white" },
+                tabBarIcon: ({ focused }) =>
+                    focused ? (
+                        <Ionicons name="person" size={24} color="black" />
+                    ) : (
+                        <Ionicons name="person-outline" size={24} color="black" />)
+            }} />
         </Tab.Navigator>
     )
 }
 
-const Stack =  createNativeStackNavigator();
-function Navigation () {
+const Stack = createNativeStackNavigator();
+function Navigation() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name='Main' component={BottomTabs} options={{headerShown:false}}/>
+                <Stack.Screen 
+                    name='Main' 
+                    component={BottomTabs} 
+                    options={{ headerShown: false }} 
+                />
+                
             </Stack.Navigator>
         </NavigationContainer>
     )
