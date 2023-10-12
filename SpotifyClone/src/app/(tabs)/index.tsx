27bce46841201efa72f4eac99b445759f72f63c3
hieu4399243 <hieu243268@gmail.com>
2023-10-13 01:08:10 +0700

    inti and clean

---
+++ SpotifyClone/src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '../../components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View>
+      <Text>Hieu</Text>
+    </View>
+  );
+}
---
+++ SpotifyClone/src/app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '../../components/Themed';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text>Tab Two</Text>
+    </View>
+  );
+}
+
