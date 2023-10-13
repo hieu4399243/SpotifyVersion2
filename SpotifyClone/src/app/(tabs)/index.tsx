70e40029d78188718552de544e4e5475cb0eeb9f
hieu4399243 <hieu243268@gmail.com>
2023-10-14 00:23:24 +0700

    Home and tracklist

--- SpotifyClone/src/app/(tabs)/index.tsx
+++ SpotifyClone/src/app/(tabs)/index.tsx
@@ -1,10 +1,23 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '../../components/Themed';
+import { StyleSheet, FlatList } from "react-native";
+import { Text, View } from "../../components/Themed";
+import { tracks } from "../../../assets/data/tracks";
+import TrackListItem from "../../components/TrackListItem";
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <Text>Hieu</Text>
-    </View>
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track ={item}/>
+      }
+      showsVerticalScrollIndicator={false}
+      />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+});
--- SpotifyClone/src/app/(tabs)/search.tsx
+++ SpotifyClone/src/app/(tabs)/search.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { Text, View } from '../../components/Themed';
+import {tracks} from '../../../assets/data/tracks';
 
 export default function TabTwoScreen() {
   return (
