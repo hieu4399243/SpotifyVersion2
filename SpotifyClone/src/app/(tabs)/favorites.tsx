1d68b782f823b569958a7e9704034f98e372905b
hieu4399243 <hieu243268@gmail.com>
2023-10-14 00:41:44 +0700

    search vs favorites

--- SpotifyClone/src/app/(tabs)/favorites.tsx
+++ SpotifyClone/src/app/(tabs)/favorites.tsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { tracks } from "../../../assets/data/tracks";
+import TrackListItem from "../../components/TrackListItem";
 
 export default function Favorites() {
   return (
-    <SafeAreaView>
-      <Text>favorites</Text>
-    </SafeAreaView>
+    <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track ={item}/>
+      }
+      showsVerticalScrollIndicator={false}
+      />
   )
 }
 
--- SpotifyClone/src/app/(tabs)/search.tsx
+++ SpotifyClone/src/app/(tabs)/search.tsx
@@ -1,12 +1,46 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '../../components/Themed';
-import {tracks} from '../../../assets/data/tracks';
+import { StyleSheet, FlatList, TextInput } from "react-native";
+import { Text, View } from "../../components/Themed";
+import { tracks } from "../../../assets/data/tracks";
+import TrackListItem from "../../components/TrackListItem";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
 
 export default function TabTwoScreen() {
+  const [search, setSearch] = useState('');
   return (
-    <View>
-      <Text>Tab Two</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        {/* Header */}
+        <FontAwesome name="search" size={16} color="gray" />
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="What do you want to listen to?"
+          style={styles.input}
+        />
+        <Text style={{ color: "black" }} onPress={() => setSearch('')}>Cancel</Text>
+      </View>
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </SafeAreaView>
   );
 }
-
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "#121314",
+    padding: 8,
+    marginHorizontal: 10,
+    borderRadius: 5,
+    color: "white",
+  },
+});
